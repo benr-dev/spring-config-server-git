a1f1565fefd0903e07725c390421f702a67dda8b
ben <ben@devbox>
2020-02-14 11:36:14 +0000

    Added Cloud Config client to app

--- demo-app/src/main/java/com/example/app/AppRestController.java
+++ demo-app/src/main/java/com/example/app/AppRestController.java
@@ -1,13 +1,17 @@
 package com.example.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class AppRestController {
 
+    @Value("${welcome-message}")
+    private String welcomeMessage;
+
     @GetMapping(path = "/")
     public String home() {
-        return "Hello World!";
+        return welcomeMessage;
     }
 }
